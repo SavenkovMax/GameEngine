83ed6a85735c522d7725fce8220b5734d805c92f
savenkovmax@outlook.com <savenkovmax@outlook.com>
2024-06-05 16:26:07 +0600

    Configuring the project structure

---
+++ Core/src/GameEngineCore/Utils/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "GameEngineCore/Utils/test.hpp"
+
+namespace GameEngine {
+	void SayHello() {
+		std::cout << "Hello" << std::endl;
+	}
+}
---
+++ Editor/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <GameEngineCore/Utils/test.hpp>
+
+int main() {
+	std::cout << "Hello, world" << std::endl;
+	GameEngine::SayHello();
+	std::cin.get();
+}
